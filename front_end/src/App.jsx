f7013af3c16702cf32c913014c2e44c1bf173281
Felabs1 <felixawere01@gmail.com>
2022-11-27 14:21:51 +0300

    create skeleton

---
+++ front_end/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css';
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+    Near Hungerthon Init
+  </h1>
+    </div>
+  )
+}
+
+export default App
