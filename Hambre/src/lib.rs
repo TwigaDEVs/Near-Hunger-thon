11ccf10a6059a50718db5ce9eae0ee27489cddb0
Felabs1 <felixawere01@gmail.com>
2022-11-27 14:32:19 +0300

    add contract skeleton

--- Hambre/src/lib.rs
+++ Hambre/src/lib.rs
@@ -1,3 +1,9 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, Promise};
+use std::collections::HashMap;
+
+pub type AccountId = String;
 #[cfg(test)]
 mod tests {
     #[test]
